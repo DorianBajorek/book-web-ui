8309bea0b6a3458483e391d427669192376eeef9
DorianBajorek <dorianb324@gmail.com>
2025-01-16 19:33:20 +0100

    contact + faq

--- src/components/ContactView.tsx
+++ src/components/ContactView.tsx
@@ -18,6 +18,35 @@ const ContactView: React.FC = () => {
             <strong>Telefon:</strong> <a href="tel:+48530421473" style={styles.link}>+48 530 421 473</a>
           </p>
         </div>
+
+        <div style={styles.privacyContainer}>
+          <h2 style={styles.heading}>Polityka Prywatności</h2>
+          <p style={styles.privacyText}>
+            Twoje dane osobowe są dla nas ważne. Korzystając z naszej platformy, akceptujesz warunki niniejszej Polityki Prywatności. 
+            Zebrane dane są wykorzystywane wyłącznie w celu świadczenia naszych usług, ulepszania funkcjonalności strony oraz wspierania użytkowników.
+          </p>
+          <h3 style={styles.subHeading}>Jakie dane zbieramy?</h3>
+          <p style={styles.privacyText}>
+            - Dane logowania (np. imię, adres e-mail).
+            <br />
+            - Informacje o Twojej aktywności na platformie.
+            <br />
+            - Inne dane niezbędne do realizacji funkcji platformy.
+          </p>
+          <h3 style={styles.subHeading}>Jak chronimy Twoje dane?</h3>
+          <p style={styles.privacyText}>
+            Stosujemy odpowiednie środki techniczne i organizacyjne, aby zapewnić bezpieczeństwo Twoich danych i chronić je przed nieuprawnionym dostępem.
+          </p>
+          <h3 style={styles.subHeading}>Twoje prawa</h3>
+          <p style={styles.privacyText}>
+            Masz prawo do wglądu w swoje dane, ich poprawiania lub usunięcia. W każdej chwili możesz skontaktować się z nami, aby skorzystać z tych praw.
+          </p>
+          <h3 style={styles.subHeading}>Jak usunąć konto?</h3>
+          <p style={styles.privacyText}>
+            Aby usunąć konto, musisz być zalogowany, wejść do sekcji Profil, kliknąć zębatkę, a następnie czerwony kosz. Opcja ta spowoduje usunięcie konta wraz ze wszystkimi informacjami. 
+            Jeśli napotkasz jakiekolwiek trudności w usuwaniu konta, skontaktuj się z naszym działem wsparcia za pośrednictwem e-maila lub telefonu podanego powyżej.
+          </p>
+        </div>
       </div>
     </div>
   );
@@ -71,6 +100,22 @@ const styles = {
     textDecoration: 'none',
     transition: 'color 0.3s',
     cursor: 'pointer' as const,
+  },
+  privacyContainer: {
+    marginTop: '30px',
+    textAlign: 'left' as const,
+  },
+  privacyText: {
+    fontSize: '16px',
+    color: '#555',
+    lineHeight: '1.6',
+    marginBottom: '20px',
+  },
+  subHeading: {
+    fontSize: '20px',
+    fontWeight: 'bold' as const,
+    color: '#333',
+    marginBottom: '10px',
   },
 };
 
